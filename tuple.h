853940db22cc50aee2b5863230498c70a0db2967
bobbyLiu <17792496233@163.com>
2024-09-22 19:29:11 +0800

    初步写tuple

--- tuple.h
+++ tuple.h
@@ -2,5 +2,21 @@
 
 namespace ldd
 {
-	
+	template<typename Head, typename... Tail>
+	class Tuple;
+
+	template<typename Head, typename... Tail>
+	class Tuple<Head, Tail...> : Tuple<Tail...>
+	{
+		typedef Tuple<Tail...> tail_tup;
+	public:
+		Tuple() {}
+		Tuple(Head h, Tail... t) : head(h), tail_tup(t...){}
+		~Tuple() = default;
+		
+		Head getHead() { return head; }
+		tile_tup& getTail() { return *this; }
+	private:
+		Head head;
+	};
 }
