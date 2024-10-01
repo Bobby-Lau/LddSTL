636186695e9c20c11739b73287c9603bc3fb23e7
bobbyLiu <17792496233@163.com>
2024-10-01 18:27:40 +0800

    递归tuple完成

--- tuple.h
+++ tuple.h
@@ -1,14 +1,20 @@
 #pragma once
-
 namespace ldd
 {
-	template<typename Head, typename... Tail>
+	//原模版
+	template<typename... Values>
 	class Tuple;
 
+	//最后一个模板，也不用传类型
+	template<>
+	class Tuple<>;
+
+	//使用递归继承实验tuple，一个tuple里可有多种不同类型
 	template<typename Head, typename... Tail>
 	class Tuple<Head, Tail...> : Tuple<Tail...>
 	{
 		typedef Tuple<Tail...> tail_tup;
+
 	public:
 		Tuple() {}
 		Tuple(Head h, Tail... t) : head(h), tail_tup(t...){}
